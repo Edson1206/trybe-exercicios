9e39d2eeec772128713dc92ce35b46ffb1bba074
Edson Pereira <ed.1206@gmail.com>
2022-05-15 23:07:50 -0300

    exercicios/4.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -114,7 +114,7 @@ console.log(numeros);
 
 //Com base na leitura que fez, considere o array numbers abaixo e faça os exercícios:
 
-//Ordene o array numbers em ordem crescente e imprima seus valores;
+//1 - Ordene o array numbers em ordem crescente e imprima seus valores;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
@@ -130,7 +130,7 @@ for (let index = 1; index < numbers.length; index += 1) {
 
 console.log(numbers);
 
-//Ordene o array numbers em ordem decrescente e imprima seus valores;
+//2 - Ordene o array numbers em ordem decrescente e imprima seus valores;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
@@ -146,7 +146,7 @@ for (let index = 1; index < numbers.length; index += 1) {
 
 console.log(numbers);
 
-//Agora crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+//3 - Agora crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let novoArray = [];
